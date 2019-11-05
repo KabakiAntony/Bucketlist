d7815127f4efdef712a08890f7a077e430c0d718
Kabaki Antony <kabaki.antony@gmail.com>
2019-11-05 13:08:21 +0300

    Setting up the repository

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    """This is the root of this application"""
+    return '<h2 style="color:white;text-align:center;background:black;">Hello and welcome to Bucketlist\
+        done by <a style="color:white;" href="https://twitter.com/kabakikiarie">Kabaki Kiarie</a></h2>\
+        <h3 style="text-align:center;">To access resources use the endpoint /lists</h3>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
