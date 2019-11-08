b5831bf09d36e0ca5685f910b14f27fd8202fbe4
Kabaki Antony <kabaki.antony@gmail.com>
2019-11-08 17:31:19 +0300

    Adding the configuration file

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config(object):
     DEBUG = False
     TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-class ProductionConfig(Config):
-    pass
+class Production(Config):
+    DATABASE_URI = os.environ.get('PROD_DATABASE_URL')
 
-class DevelopmentConfig(Config):
+class Development(Config):
     DEBUG = True
 
-class TestingConfig(Config):
+class Testing(Config):
+    DEBUG = True
     TESTING = True
+    DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
