0fc1b34919386418329853c178159c1692e51943
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-12 23:13:16 +0300

    Made changes to UI

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,8 +1,21 @@
-document.getElementById("sign-in").addEventListener('click',
+/* this part acts on the sign in modal */
+
+document.getElementById ('sign-in').addEventListener('click',
 function(){
-    document.querySelector('.bg-modal').style.display = "flex";
+    document.querySelector('.bg-sigin-modal').style.display = "flex";
 });
 document.querySelector('.close').addEventListener('click',
 function(){
-    document.querySelector('.bg-modal').style.display = "none";
-})
+    document.querySelector('.bg-sigin-modal').style.display = "none";
+});
+
+/* this part acts on the  sign up modal */
+
+document.getElementById("sign-up").addEventListener('click',
+function(){
+    document.querySelector('.bg-register-modal').style.display = "flex";
+});
+document.querySelector('.closeb').addEventListener('click',
+function(){
+    document.querySelector('.bg-register-modal').style.display = "none";
+});
