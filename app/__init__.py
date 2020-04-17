ae5903ae93aa1a6ae58fba5e6c00dbc651bd4ef6
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-17 12:15:45 +0300

    Added cors to the app

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from config import app_config
 from app.api.views.lists import bucket_list as lists_blueprint
 from app.api.views.users import bucket_list as users_blueprint
 from app.api.models.db import db_init
+from flask_cors import CORS
 
 def create_app(the_configuration):
     """This is where the app is created for this application"""
     app = Flask(__name__)
+    #enable cross origin rekuests
+    CORS(app)
     app.config.from_object(app_config[the_configuration])
     app.app_context().push()
     app.register_blueprint(lists_blueprint)
