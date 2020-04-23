dd2c87b10b812541810ce4e0a4c5860277d059b0
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-23 20:31:32 +0300

    Modified sign up js

--- UI/js/main.js
+++ UI/js/main.js
@@ -9,11 +9,6 @@ let signInForm = document.getElementById('inForm');
 let inEmail = document.getElementById('inEmail');
 let inPassword = document.getElementById('inPassword');
 
-/* values for error reporting */
-const errorDiv = document.getElementById('error');
-let emailRegex =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
-let feedback = [];
-
 /* this part acts on the sign in modal */
 
 document.getElementById ('sign-in').addEventListener('click',
@@ -36,10 +31,19 @@ function(){
     document.querySelector('.bg-register-modal').style.display = "none";
 });
 
-/* validation on submit during signup goes here */
 
 signUpForm.addEventListener('submit',(e)=>{
     e.preventDefault();
+    validateData();
+    postSignUp();
+});
+
+function validateData(){
+    /* validation on submit during signup goes here */
+    let errorDiv = document.getElementById('upError');
+    let emailRegex =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
+    let feedback = [];
+    
 
     if(upFirstName.value === '' || upFirstName.value == null){
         feedback.push('Enter a name');
@@ -47,7 +51,7 @@ signUpForm.addEventListener('submit',(e)=>{
     }
     if(!upEmail.value.match(emailRegex)||upEmail.value === ""){
         feedback.push('Enter a valid email');
-        document.getElementById("upEmail").style.borderColor = "red";      
+        document.getElementById("upEmail").style.borderColor = "red";    
     }
     if(upPassword.value.length < 6){
         document.getElementById("upPassword").style.borderColor = "red";
@@ -56,48 +60,51 @@ signUpForm.addEventListener('submit',(e)=>{
     if(upPassword.value.length > 20){
         document.getElementById("upPassword").style.borderColor = "red";
         feedback.push('Password too long!');
-
     }
     if (feedback.length > 0){
-        e.preventDefault();
         errorDiv.innerText = feedback.join(' , ');
     }
-
+}
+function postSignUp(){
     /* when validations are ok  then go ahead and sign up user with entered data */
-    
-    const email = document.getElementById('upEmail').value;
-    const password = document.getElementById('upPassword').value;
-    const firstname = document.getElementById('firstname').value;
-    signUpData = {
-        firstname,
-        email,
-        password
-    };
-    //console.log(signUpData);
-    fetch('https://kabucketlist.herokuapp.com/auth/signup',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(signUpData)
-      })
-      .then(response => {
-        // Here we get the response from the server
-        if(response.ok) {
-            return response.json();
-        }
-        else
-        {
-            console.log("The server responded with a status of : ", response.status);
-        }
-    })
-    .then(data => {
-        // right here you do what you want with the data at 
-        // this point we are only going to send
-        // it to the console
-        console.log(data);
-    })
-    // this error only occurs if there is something that is preventing
-    // a fetch most of the times it is a network error.
-    .catch(error => console.log('This error occured :',error));        
-});
+
+const email = document.getElementById('upEmail').value;
+const password = document.getElementById('upPassword').value;
+const firstname = document.getElementById('firstname').value;
+signUpData = {
+    firstname,
+    email,
+    password
+};
+fetch('https://kabucketlist.herokuapp.com/auth/signup',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(signUpData)
+  })
+  .then(response => {
+    /* 
+    Here we get the response from the server
+    That is a success or why it is not.
+    */ 
+    if(response.ok) {
+        return response.json();          
+    }
+    else
+    {
+        console.log("The server could not service our rekuest due to : ", response.statusText);
+    }
+})
+.then(data => {
+    /*
+    This returns the data we posted to the server
+    */
+    console.log(data);
+})
+/* 
+An error here only occurs if there is something that is 
+preventing a fetch most of the times it is a network error.
+ */
+.catch(error => console.log('This error occured :',error));
+}
