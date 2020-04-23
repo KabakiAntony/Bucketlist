4de7291f8b318d9b64ecd06c991eeebd4d435837
Kabaki Antony <kabaki.antony@gmail.com>
2020-04-23 18:29:47 +0300

    Added signup js

---
+++ UI/js/main.js
@@ -0,0 +1,102 @@
+/* values for sign-up */
+let signUpForm = document.getElementById('upForm');
+let upFirstName = document.getElementById('firstname'); 
+let upEmail = document.getElementById('upEmail');
+let upPassword = document.getElementById('upPassword');
+
+/* values for sign-in */
+let signInForm = document.getElementById('inForm');
+let inEmail = document.getElementById('inEmail');
+let inPassword = document.getElementById('inPassword');
+
+/* values for error reporting */
+const errorDiv = document.getElementById('error');
+let emailRegex =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
+let feedback = [];
+
+/* this part acts on the sign in modal */
+
+document.getElementById ('sign-in').addEventListener('click',
+function(){
+    document.querySelector('.bg-sigin-modal').style.display = "flex";
+});
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.bg-sigin-modal').style.display = "none";
+});
+
+/* this part acts on the  sign up modal */
+
+document.getElementById("sign-up").addEventListener('click',
+function(){
+    document.querySelector('.bg-register-modal').style.display = "flex";
+});
+document.querySelector('.closeb').addEventListener('click',
+function(){
+    document.querySelector('.bg-register-modal').style.display = "none";
+});
+
+/* validation on submit during signup goes here */
+
+signUpForm.addEventListener('submit',(e)=>{
+
+    if(upFirstName.value === '' || upFirstName.value == null){
+        feedback.push('Enter a name');
+        document.getElementById("firstname").style.borderColor = "red";
+    }
+    if(!upEmail.value.match(emailRegex)||upEmail.value === ""){
+        feedback.push('Enter a valid email');
+        document.getElementById("upEmail").style.borderColor = "red";      
+    }
+    if(upPassword.value.length < 6){
+        document.getElementById("upPassword").style.borderColor = "red";
+        feedback.push('Password too short!');
+    }
+    if(upPassword.value.length > 20){
+        document.getElementById("upPassword").style.borderColor = "red";
+        feedback.push('Password too long!');
+
+    }
+    if (feedback.length > 0){
+        e.preventDefault();
+        errorDiv.innerText = feedback.join(' , ');
+    }
+
+    /* when validations are ok  then go ahead and sign up user with entered data */
+    
+    const email = document.getElementById('upEmail').value;
+    const password = document.getElementById('upPassword').value;
+    const firstname = document.getElementById('firstname').value;
+    signUpData = {
+        firstname,
+        email,
+        password
+    };
+    //console.log(signUpData);
+    fetch('https://kabucketlist.herokuapp.com/auth/signup',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signUpData)
+      })
+      .then(response => {
+        // Here we get the response from the server
+        if(response.ok) {
+            return response.json();
+        }
+        else
+        {
+            console.log("The server responded with a status of : ", response.status);
+        }
+    })
+    .then(data => {
+        // right here you do what you want with the data at 
+        // this point we are only going to send
+        // it to the console
+        console.log(data);
+    })
+    // this error only occurs if there is something that is preventing
+    // a fetch most of the times it is a network error.
+    .catch(error => console.log('This error occured :',error));        
+});
--- UI/js/validate.js
+++ UI/js/validate.js
@@ -1,28 +1,39 @@
 /* this file is meant for various js validations */
-const singUpForm = document.getElementById('upForm');
-const firstname = document.getElementById('firstname'); 
-const email = document.getElementById('upEmail');
-const password = document.getElementById('upPassword');
+/* constants for signup modal */
+const signUpForm = document.getElementById('upForm');
+const upFirstName = document.getElementById('firstname'); 
+const upEmail = document.getElementById('upEmail');
+const upPassword = document.getElementById('upPassword');
 const errorDiv = document.getElementById('error');
 let emailRegex =/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
 let feedback = [];
-
-singUpForm.addEventListener('submit',(e)=>{
-
-    if(firstname.value === '' || firstname.value == null){
+/* constants for the signin modal
+const signInForm = document.getElementById('inForm');
+const inEmail = document.getElementById('inEmail');
+const inPassword = document.getElementById('inPassword');
+/* listen for submit on the signup modal */
+async function getUsers() 
+    {
+      let response = await fetch(`https://kabucketlist.herokuapp.com/users`);
+      let data = await response.json();
+      return data;
+    }
+signUpForm.addEventListener('submit',(e)=>{
+    /*
+ 
+    if(upFirstName.value === '' || upFirstName.value == null){
         feedback.push('Enter a name');
         document.getElementById("firstname").style.borderColor = "red";
     }
-    if(!email.value.match(emailRegex)||email.value === ""){
+    if(!upEmail.value.match(emailRegex)||email.value === ""){
         feedback.push('Enter a valid email');
         document.getElementById("upEmail").style.borderColor = "red";      
     }
-    if(password.value.length < 6){
+    if(upPassword.value.length < 6){
         document.getElementById("upPassword").style.borderColor = "red";
         feedback.push('Password too short!');
-
     }
-    if(password.value.length > 20){
+    if(upPassword.value.length > 20){
         document.getElementById("upPassword").style.borderColor = "red";
         feedback.push('Password too long!');
 
@@ -31,6 +42,66 @@ singUpForm.addEventListener('submit',(e)=>{
         e.preventDefault();
         errorDiv.innerText = feedback.join(' , ');
     }
+
+   /* when all else above is true go ahead and signup user 
+   fetch('https://kabucketlist.herokuapp.com/users')
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+  .catch((err)=> console.log(err));*/
+
+/*
     
-    
+    fetch('https://kabucketlist.herokuapp.com/auth/signup',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signUpData)
+      })
+      .then((response) => response.json())
+      .then((Data)=>{
+          console.log(Data);
+          /*if(!Status === 201 ){
+              console.log('User signup successfully.'+ Data);
+              alert(Error);
+          }
+          else{
+              console.log(Error);
+              alert('Failed');
+          }
+
+      }).catch((error) =>{
+          
+          alert("An error occured "+ error);
+          console.log("This error "+ error);
+      });*/
+      getUsers()
+      .then(data => console.log(data)).catch(error =>{
+        console.log('THE FOLLOWING ERROR OCCURED',error);
+        alert("Lets see this error");
+      });
 });
+
+
+/* listen for submit on the signin form  
+signInForm.addEventListener('submit',(e)=>{
+
+    if(!inEmail.value.match(emailRegex)||inEmail.value === ""){
+        feedback.push('Enter a valid email');
+        document.getElementById("inEmail").style.borderColor = "red";      
+    }
+    if(inPassword.value.length < 6){
+        document.getElementById("inPassword").style.borderColor = "red";
+        feedback.push('Password too short!');
+
+    }
+    if(inPassword.value.length > 20){
+        document.getElementById("inPassword").style.borderColor = "red";
+        feedback.push('Password too long!');
+
+    }
+    if (feedback.length > 0){
+        e.preventDefault();
+        errorDiv.innerText = feedback.join(' , ');
+    }  
+});*/
