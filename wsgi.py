b6b70a016499e9d5128d3bc5f65f7503a9119866
Kabaki Antony <kabaki.antony@gmail.com>
2020-06-02 14:24:26 +0300

    Added index template

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import create_app
+from flask import render_template
 
 the_configuration = "production"
 app = create_app(the_configuration)
@@ -6,9 +7,7 @@ app = create_app(the_configuration)
 @app.route('/')
 def root():
     """This is the root of this application"""
-    return '<h2 style="color:white;text-align:center;background:black;">Hello and welcome to Bucketlist\
-        done by <a style="color:white;" href="https://twitter.com/kabakikiarie">Kabaki Kiarie</a></h2>\
-        <h3 style="text-align:center;">To access resources use the endpoint /lists</h3>'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
