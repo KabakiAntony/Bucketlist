f953ef38a52394436a0d474b8f8ba67f94db5c6e
Kabaki Antony <kabaki.antony@gmail.com>
2020-06-05 14:44:53 +0300

    Removed some routes from the entry app

--- wsgi.py
+++ wsgi.py
@@ -9,17 +9,6 @@ def root():
     """This is the root of this application"""
     return render_template('index.html')
 
-@app.route('/u/signup')
-def user_signin():
-    """Return the user sign up page"""
-    return render_template('signup.html')
-
-@app.route('/contact')
-def contact():
-    """Return the contact us page"""
-    return render_template('contact.html')
-
-
 
 if __name__ == "__main__":
     app.run()
