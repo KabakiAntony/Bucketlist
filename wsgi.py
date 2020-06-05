5913a6737d4b9e8bccaa35efe3b0f2613a2c0156
Kabaki Antony <kabaki.antony@gmail.com>
2020-06-05 12:52:50 +0300

    Changed signin to signup route

--- wsgi.py
+++ wsgi.py
@@ -9,10 +9,10 @@ def root():
     """This is the root of this application"""
     return render_template('index.html')
 
-@app.route('/u/signin')
+@app.route('/u/signup')
 def user_signin():
-    """Return the user sign in page"""
-    return render_template('signin.html')
+    """Return the user sign up page"""
+    return render_template('signup.html')
 
 @app.route('/contact')
 def contact():
