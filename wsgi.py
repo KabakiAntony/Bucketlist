f66bc24e98395d4d6bfb74b765eb2eacd6a38885
Kabaki Antony <kabaki.antony@gmail.com>
2020-06-05 12:49:09 +0300

    Add signin and contact routes

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,18 @@ def root():
     """This is the root of this application"""
     return render_template('index.html')
 
+@app.route('/u/signin')
+def user_signin():
+    """Return the user sign in page"""
+    return render_template('signin.html')
+
+@app.route('/contact')
+def contact():
+    """Return the contact us page"""
+    return render_template('contact.html')
+
+
+
 if __name__ == "__main__":
     app.run()
 
