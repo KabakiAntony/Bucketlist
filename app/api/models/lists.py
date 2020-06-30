f68a427c1bd173922d42c918f0903f7ad5f6a259
Kabaki Antony <kabaki.antony@gmail.com>
2020-06-30 19:13:14 +0300

    Changed to use f strings on list.py models

--- app/api/models/lists.py
+++ app/api/models/lists.py
@@ -11,13 +11,13 @@ class Lists:
     def create_post_item(self):
         """create/add a new bucket list item to the
         database"""
-        insert_new_post = """
+        insert_new_post = f"""
         INSERT INTO posts(content,user_id,isStarted,isCompleted)
-        VALUES ('{}','{}','true','false') RETURNING post_id;
-        """.format(self.content,self.user_id)
+        VALUES ('{self.content}','{self.user_id}','true','false') RETURNING post_id;
+        """
         return db.handle_other_queries(insert_new_post,True)
 
-    def format_lists(iterable):
+    def format_lists(self,iterable):
         """format the list to make it a dict"""
         list_data = []
         for list_item in iterable:
@@ -31,36 +31,36 @@ class Lists:
             list_data.append(list_format)
         return list_data
 
-    def get_all_post_items(user_id):
+    def get_all_post_items(self,user_id):
         """Getting all list items"""
-        get_all_posts = """
+        get_all_posts = f"""
         SELECT post_id, content,user_id,isStarted,isCompleted from posts
-        WHERE posts.user_id ='{}'""".format(user_id)
+        WHERE posts.user_id ='{user_id}'"""
         return Lists.format_lists(db.handle_select_queries(get_all_posts))
 
-    def get_a_single_post(user_id,post_id):
+    def get_a_single_post(self,user_id,post_id):
         """Getting all list items"""
-        get_a_single_post_item = """
+        get_a_single_post_item = f"""
         SELECT post_id, content, user_id,isStarted,isCompleted from posts
-        WHERE posts.post_id ={} AND posts.user_id={}""".format(post_id,user_id)
+        WHERE posts.post_id ={post_id} AND posts.user_id={user_id}"""
         return Lists.format_lists(db.handle_select_queries(get_a_single_post_item))
 
-    def update_a_post(user_id,post_id,update_content):
+    def update_a_post(self,user_id,post_id,update_content):
         """Updating a list item"""
-        updating_post_item = """
-        UPDATE posts SET  content ='{}' WHERE posts.post_id ={}
-        AND posts.user_id={}
-        """.format(update_content,post_id,user_id)
+        updating_post_item = f"""
+        UPDATE posts SET  content ='{update_content}' WHERE posts.post_id ={post_id}
+        AND posts.user_id={user_id}
+        """
         if Lists.get_a_single_post(user_id,post_id):
             db.handle_other_queries(updating_post_item)
             return Lists.get_a_single_post(user_id,post_id)    
     
-    def delete_a_post(user_id,post_id):
+    def delete_a_post(self,user_id,post_id):
         """Updating a list item"""
         deleting_a_post_item = """
-        DELETE FROM posts  WHERE posts.post_id ={}
-        AND posts.user_id={}
-        """.format(post_id,user_id)
+        DELETE FROM posts  WHERE posts.post_id ={post_id}
+        AND posts.user_id={user_id}
+        """
         if Lists.get_a_single_post(user_id,post_id):
             db.handle_other_queries(deleting_a_post_item)
-            return "Post id {} deleted successfully.".format(post_id)
+            return f"Post id {post_id} deleted successfully."
